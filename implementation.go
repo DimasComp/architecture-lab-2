2903a2548a1658c5e7a8b328eb554bd6292950f5
Demchyk Dmytro <d.demchyk.im21@kpi.ua>
2024-03-19 22:58:48 +0200

    make postfix to infix function

--- implementation.go
+++ implementation.go
@@ -1,9 +1,38 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+func isOperator(s string) bool {
+	return s == "+" || s == "-" || s == "*" || s == "/"
+}
+
+func PostfixToInfix(postfix string) (string, error) {
+	stack := []string{}
+	tokens := strings.Fields(postfix)
+
+	for _, token := range tokens {
+		if !isOperator(token) {
+			stack = append(stack, token)
+		} else {
+			if len(stack) < 2 {
+				return "", fmt.Errorf("invalid postfix expression")
+			}
+			op2 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			op1 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			infix := fmt.Sprintf("(%s %s %s)", op1, token, op2)
+			stack = append(stack, infix)
+		}
+	}
+
+	if len(stack) != 1 {
+		return "", fmt.Errorf("invalid postfix expression")
+	}
+
+	return stack[0], nil
 }
