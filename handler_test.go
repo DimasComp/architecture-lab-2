35cccf53e288b7fad94a5816d33ce9e97ac2f446
daniilchubich <Daniilchubich@gmail.com>
2024-03-20 09:12:59 +0200

    add error test

---
+++ handler_test.go
@@ -0,0 +1,35 @@
+package lab2_test
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/DimasComp/architecture-lab-2"
+)
+
+func TestComputeHandler(t *testing.T) {
+
+	t.Run("Write test", func(t *testing.T) {
+        input := "2 3 + 4 5 * -"
+        expected := "2 + 3 - 4 * 5"
+		buf := bytes.NewBuffer([]byte{})
+
+		handler := lab2.ComputeHandler{Input: strings.NewReader(input), Output: buf}
+		err := handler.Compute()
+		result := buf.String()
+        assert.NoError(t, err)
+        assert.Equal(t, expected, result)
+    })
+
+	t.Run("Error test", func(t *testing.T) {
+        input := "2 3 2 $"
+		buf := bytes.NewBuffer([]byte{})
+
+		handler := lab2.ComputeHandler{Input: strings.NewReader(input), Output: buf}
+		err := handler.Compute()
+        assert.Error(t, err)
+    })
+}
