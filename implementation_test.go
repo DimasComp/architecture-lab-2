e021349f29b20cf428df630550dfbebe8e4fe893
daniilchubich <Daniilchubich@gmail.com>
2024-03-20 08:37:47 +0200

    fix test import

--- implementation_test.go
+++ implementation_test.go
@@ -3,14 +3,14 @@ package lab2_test
 import (
     "testing"
     "github.com/stretchr/testify/assert"
-    yourpackage "github.com/DimasComp/architecture-lab-2"
+    "github.com/DimasComp/architecture-lab-2"
 )
 
 func TestPostfixToInfix(t *testing.T) {
     t.Run("Three operations", func(t *testing.T) {
         input := "2 3 + 4 5 * -"
         expected := "2 + 3 - 4 * 5"
-        result, err := yourpackage.PostfixToInfix(input)
+        result, err := lab2.PostfixToInfix(input)
         assert.NoError(t, err)
         assert.Equal(t, expected, result)
     })
@@ -18,7 +18,7 @@ func TestPostfixToInfix(t *testing.T) {
 	t.Run("Four operations", func(t *testing.T) {
         input := "1 3 2 * + 6 2 / -"
         expected := "1 + 3 * 2 - 6 / 2"
-        result, err := yourpackage.PostfixToInfix(input)
+        result, err := lab2.PostfixToInfix(input)
         assert.NoError(t, err)
         assert.Equal(t, expected, result)
     })
@@ -26,7 +26,7 @@ func TestPostfixToInfix(t *testing.T) {
 	t.Run("Eight operations", func(t *testing.T) {
         input := "1 2 + 3 4 * 5 / - 6 - 7 8 / - 9 +"
         expected := "1 + 2 - 3 * 4 / 5 - 6 - 7 / 8 + 9"
-        result, err := yourpackage.PostfixToInfix(input)
+        result, err := lab2.PostfixToInfix(input)
         assert.NoError(t, err)
         assert.Equal(t, expected, result)
     })
@@ -34,14 +34,14 @@ func TestPostfixToInfix(t *testing.T) {
     t.Run("Just one number", func(t *testing.T) {
         input := "7"
         expected := "7"
-        result, err := yourpackage.PostfixToInfix(input)
+        result, err := lab2.PostfixToInfix(input)
         assert.NoError(t, err)
         assert.Equal(t, expected, result)
     })
 
     t.Run("Invalid characters", func(t *testing.T) {
         input := "2 @ 3 +"
-        _, err := yourpackage.PostfixToInfix(input)
+        _, err := lab2.PostfixToInfix(input)
         assert.Error(t, err)
     })
 }
